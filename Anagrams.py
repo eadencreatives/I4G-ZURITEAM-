5d97e43dc655b981adfed8fbb3b03a58673f6f3e
eadencreatives <81325125+eadencreatives@users.noreply.github.com>
2022-05-28 14:41:16 +0100

    Update Anagrams.py

--- Anagrams.py
+++ Anagrams.py
@@ -22,3 +22,23 @@ def find_anagram(word, anagram):
         return False
 
 print(find_anagram('hello' , 'check'))
+
+
+def find_anagram(word, anagram):
+    str1 = "below"
+    str2 = "elbow"
+
+    str1 = str1.lower()
+    str2 = str2.lower()
+
+    if(len(str1) == len(str2)):
+
+        sorted_str1 = sorted(str1)
+    sorted_str2 = sorted(str2)
+
+    if(sorted_str1 == sorted_str2):
+        return True
+    else:
+        return False
+
+print(find_anagram('below' , 'elbow'))
