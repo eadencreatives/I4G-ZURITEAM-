ccbd209cde486c1906465f9432f9e60677f5c785
eadencreatives <81325125+eadencreatives@users.noreply.github.com>
2022-06-03 04:49:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def read_file_content(filename):
     # [assignment] Add your code here 
-    f = open('story.txt', 'r')
-    filename = f.read()
+    f = open(filename)
+    file_content = f.read()
 
     return filename
-print(read_file_content('filename'))
 
 
 def count_words():
-    text = read_file_content('story.txt')
+    text = read_file_content('./story.txt')
     # [assignment] Add your code here
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    count = {}
     words_of_text = text.split()
-    count = dict()
+    
     for word in words_of_text :
         if word in count:
             count[word] += 1
@@ -19,5 +20,5 @@ def count_words():
             count[word] = 1
 
     return count
-a = count_words()
-print(a)
+
+print(count_words())
